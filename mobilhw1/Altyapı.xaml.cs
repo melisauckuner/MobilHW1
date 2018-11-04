991e65270e7a4c78c309aec9ac6b5ddbb25223fc
melisauckuner <32360262+melisauckuner@users.noreply.github.com>
2018-11-04 23:17:33 +0300

    Add files via upload

---
+++ mobilhw1/Altyapı.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mobilhw1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Altyapı : ContentPage
+    {
+
+
+
+        public Altyapı()
+
+        {
+
+            InitializeComponent();
+        }
+    }
+}
+
+   
